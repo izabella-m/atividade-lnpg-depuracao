4628b5922494f431e35ba115172098d2dd330faa
izabella-m <izabella.pessoa03@outlook.com>
2024-07-17 23:02:17 -0300

    Fourth Problem

--- problem_04.py
+++ problem_04.py
@@ -47,9 +47,11 @@ class Bank:
         return None
 
     def transfer(self, from_account, to_account, amount):
-        from_account.withdraw(amount)  
-        to_account.deposit(amount)
-
+        if from_account.get_balance() >= amount:  #verifica se a conta tem saldo suficiente para fazer a transação
+            from_account.withdraw(amount)  
+            to_account.deposit(amount)
+        else:
+            print("A tranferencia falhou, saldo insuficiente")  # Retorno para transferência inválida
 
 def main():
     # Create a bank
